86e7f3e2c0ddc21a72f2108ca45dbd522a3b27de
Karlingas <dgdcuenca@gmail.com>
2025-09-23 16:56:19 +0200

    ej3 y ej4

---
+++ arbol.py
@@ -0,0 +1,56 @@
+class arbol:
+    def __init__(self, elto):
+        self.elto = elto
+        self.izdo = None
+        self.dcho = None
+    
+    def Inserta(self, value):
+        if value < self.elto:
+            if self.izdo is None:
+                self.izdo = arbol(value)
+            else:
+                self.izdo.Inserta(value)
+        elif value > self.elto:
+            if self.dcho is None:
+                self.dcho = arbol(value)
+            else:
+                self.dcho.Inserta(value)
+        else:
+            print("El elemento ya está en el árbol, no se permiten duplicados.")
+            return
+    
+    def Esta(self, value):
+        if value == self.elto:
+            return True
+        elif value < self.elto:
+            if self.izdo is None:
+                return False
+            else:
+                return self.izdo.Esta(value)
+        else:
+            if self.dcho is None:
+                return False
+            else:
+                return self.dcho.Esta(value)
+    
+    def __str__(self):
+        resultado = ""
+        if self.izdo is not None:
+            resultado += str(self.izdo) + ", "
+        resultado += str(self.elto)
+        if self.dcho is not None:
+            resultado += ", " + str(self.dcho)
+        return resultado
+    
+    def Modificar(self, viejo, nuevo):
+        if viejo == self.elto:
+            self.elto = nuevo
+        elif viejo < self.elto:
+            if self.izdo is not None:
+                self.izdo.Modificar(viejo, nuevo)
+        else:
+            if self.dcho is not None:
+                self.dcho.Modificar(viejo, nuevo)
+    
+    def Valor(self):
+        return self.elto
---
+++ asst.py
@@ -0,0 +1,60 @@
+class AST:
+    def __init__(self): pass
+    def mostrar(self): pass             # muestra la expresion utilizando parentesis
+    def numero_operaciones(self): pass  # muestra el numero de operaciones en la expresion
+    def interpreta(self): pass          # evalua la expresion
+
+class Numero(AST):
+    def __init__(self, value):
+        self.value = value
+    
+    def mostrar(self):
+        return str(self.value)
+    
+    def numero_operaciones(self):
+        return 0
+    
+    def interpreta(self):
+        return self.value
+    
+class Operacion(AST):
+    def __init__(self, op, izda, dcha):
+        self.SOPORTED_OPERATORS = {'+', '-', '*', '/'}
+        self.op= op
+        self.izda= izda
+        self.dcha= dcha
+    
+    def mostrar(self):
+        return '(' + self.izda.mostrar() + self.op + self.dcha.mostrar() + ')'
+    
+    def numero_operaciones(self):
+        return 1 + self.izda.numero_operaciones() + self.dcha.numero_operaciones()
+    
+    def interpreta(self):
+        if self.SOPORTED_OPERATORS.__contains__(self.op):
+            if self.op == '+':
+                return self.izda.interpreta() + self.dcha.interpreta()
+            elif self.op == '-':
+                return self.izda.interpreta() - self.dcha.interpreta()
+            elif self.op == '*':
+                return self.izda.interpreta() * self.dcha.interpreta()
+            elif self.op == '/':
+                return self.izda.interpreta() / self.dcha.interpreta()
+        else:
+            raise Exception("Operador no soportado: " + self.op + " actualmente solo se soportan " + str(self.SOPORTED_OPERATORS))
+
+'''
+PRUEBA DEL CODIGO
+'''
+# Introducimos el ´arbol de la expresi´on 4*5+3*2
+num1=Numero(4)
+num2=Numero(5)
+num3=Numero(3)
+num4=Numero(2)
+arbol1=Operacion('*',num1,num2) # 4*5
+arbol2=Operacion('*',num3,num4) # 3*2
+arbol_final=Operacion('+',arbol1,arbol2) # arbol1+arbol2
+# Accedemos al ´arbol de tres formas diferentes mediante funciones miembro
+print('El arbol contiene la expresion:', arbol_final.mostrar())
+print('El arbol contiene en total %d operaciones' % arbol_final.numero_operaciones())
+print('La expresion se evalua como:', arbol_final.interpreta())
